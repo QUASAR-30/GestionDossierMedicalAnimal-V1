da77c06441870573da48fdd3804c699ede22b664
TIOGUIM Prince <princetioguim@gmail.com>
2025-03-11 01:00:37 +0100

    Initial commit

---
+++ src/main/java/com/iuc/gestionDossierMedicalAnimaux/GestionDossierMedicalAnimauxApplication.java
@@ -0,0 +1,13 @@
+package com.iuc.gestionDossierMedicalAnimaux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDossierMedicalAnimauxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDossierMedicalAnimauxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iuc/gestionDossierMedicalAnimaux/GestionDossierMedicalAnimauxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuc.gestionDossierMedicalAnimaux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDossierMedicalAnimauxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
